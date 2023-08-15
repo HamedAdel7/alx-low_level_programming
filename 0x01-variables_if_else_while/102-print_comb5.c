6b4a6b4a6ab5a3dee092e3c3934b6d9b9faa09b3
HamedAdel7 <hamedadel2021@gmail.com>
2023-08-14 21:12:09 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of two two-digit numbers.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 <= 99)
+	{
+		d2 = d1;
+		while (d2 <= 99)
+		{
+			if (d1 != d2)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+				if (d1 != 98 || d2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
