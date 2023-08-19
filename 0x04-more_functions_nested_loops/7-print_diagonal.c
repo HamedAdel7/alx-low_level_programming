3bce424f12e600e7b58f099da0194e7c6e385c18
HamedAdel7 <hamedadel2021@gmail.com>
2023-08-18 17:42:20 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,19 @@
 
 void print_diagonal(int n)
 {
-	int biago;
+	int diago, space;
 
 	if (n <= 0)
 		_putchar('\n');
 
-	else if (n == 1)
-	{
-		_putchar(92);
-		_putchar('\n');
-	}
 	else
 	{
-		for (biago = 1; biago <= (n - 1); biago++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+		for (diago = 1; diago <= n; diago++)
+		{
+			for (space = 1; space <= diago; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
