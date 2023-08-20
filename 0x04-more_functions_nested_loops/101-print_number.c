1e7114349611de6539594a66a9dc630e17ccaf14
HamedAdel7 <hamedadel2021@gmail.com>
2023-08-19 18:57:03 -0700

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,50 @@
 
 void print_number(int n)
 {
+	int a, s, d, f, g;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	_putchar((n % 10) + 48);
+	g = n % 10;
+	n = n / 10;
+	f = n % 10;
+	n = n / 10;
+	d = n % 10;
+	n = n / 10;
+	s = n % 10;
+	n = n / 10;
+	a = n % 10;
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
+	if (a > 0)
+	{
+		_putchar(a + 48);
+		_putchar(s + 48);
+		_putchar(d + 48);
+		_putchar(f + 48);
+		_putchar(g + 48);
+	}
+	else if (s > 0)
+	{
+		_putchar(s + 48);
+		_putchar(d + 48);
+		_putchar(f + 48);
+		_putchar(g + 48);
+	}
+	else if (d > 0)
+	{
+		_putchar(d + 48);
+		_putchar(f + 48);
+		_putchar(g + 48);
+	}
+	else if (f > 0)
+	{
+		_putchar(f + 48);
+		_putchar(g + 48);
+	}
+	else
+		_putchar(g + 48);
 }
