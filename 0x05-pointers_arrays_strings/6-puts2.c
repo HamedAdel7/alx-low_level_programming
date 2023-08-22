ef5073a6396871ac8aebbcab9fa5a1be298349cc
HamedAdel7 <hamedadel2021@gmail.com>
2023-08-21 18:51:46 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - print even char
+ * @str: input string
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
