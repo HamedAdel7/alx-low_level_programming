3fc4fe3c0a4603c16e6561b293e25e03d4923809
HamedAdel7 <hamedadel2021@gmail.com>
2023-09-06 19:23:51 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,10 @@ int main(int argc, char *argv[])
 		for (c = argv[argc]; *c; c++)
 		{
 			if (*c < '0' || *c > '9')
-				return (printf("Error\n", 1));
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[argc]);
 	}
