083ffb9771f036703ca96dd4787dd543267fe1b9
HamedAdel7 <hamedadel2021@gmail.com>
2023-09-13 06:55:45 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,10 @@
 #include "function_pointers.h"
 
-/***/
+/**
+ * print_name - print name
+ * @name: string name
+ * @f: the function pointer
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
