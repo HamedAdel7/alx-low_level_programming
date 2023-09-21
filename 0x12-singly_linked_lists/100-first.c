fdbd7f43c27407eab8cf245879c50c152706fd47
HamedAdel7 <hamedadel2021@gmail.com>
2023-09-21 14:35:47 -0700

    5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: void
 */
 
-void before_main() __attribute__((constructor))
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
