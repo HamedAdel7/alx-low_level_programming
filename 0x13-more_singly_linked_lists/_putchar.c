372dfdce6db591e83bce8e1bc8d59e76d322d29f
HamedAdel7 <hamedadel2021@gmail.com>
2023-09-26 06:28:15 -0700

    print list

--- 0x13-more_singly_linked_lists/_putchar.c
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "lists"
 
 /**
  * _putchar - print character
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
