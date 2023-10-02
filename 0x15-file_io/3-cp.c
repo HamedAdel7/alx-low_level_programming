420d7b5f2d4f640fc316d30f0da9f9242ea0327b
HamedAdel7 <hamedadel2021@gmail.com>
2023-10-02 11:21:22 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,6 +25,7 @@ int main(int ac, char **av)
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
@@ -35,9 +36,9 @@ int main(int ac, char **av)
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
 	if (from_fd)
-		dprintf(STDERR_FILENO, ERR_CLOSE, from_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_CLOSE, from_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
 	return (EXIT_SUCCESS);
 }
